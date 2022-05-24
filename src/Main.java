dd2ec10ed6f669e03c8d47af708d24c0fd371ed6
Hatice GAZEL <98157466+gazellhatice@users.noreply.github.com>
2022-05-24 14:38:37 +0300

    güncelleme

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,7 @@ public class Main {
         System.out.println("Sayı Giriniz: ");
         number = inp.nextInt();
 
+
         for (int i=0 ; i<=number ; i++){
             if(i % 3 == 0 & i % 4 == 0 ){
                 sum +=i;
